4d40aa068b54f9c02d7d59d82f2431ca61d9758e
KipCrossing <kip.crossing@gmail.com>
2020-01-05 12:18:22 +1100

    update example

--- example.py
+++ example.py
@@ -2,6 +2,5 @@ from ausbills import federal_bills
 import pandas as pd
 
 df_lower = pd.DataFrame(federal_bills.get_house_bills())
-# df_upper = pd.DataFrame(ausbills.get_senate_bills())
 
 print(df_lower)
