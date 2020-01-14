0b149ee9b2eb760b1fcf1ff77d6fe96d63fced0c
KipCrossing <kip.crossing@gmail.com>
2020-01-14 19:25:49 +1100

    get_bill_text_links(url) returns 3 links

--- ausbills/federal_bills.py
+++ ausbills/federal_bills.py
@@ -46,6 +46,17 @@ def get_bill_summary(bill_url_string):
     return(summary)
 
 
+def get_bill_text_links(bill_url_string):
+    try:
+        bill_url = requests.get(bill_url_string).text
+        bill_soup = BeautifulSoup(bill_url, 'lxml')
+        tr = bill_soup.find("tr", id='main_0_textOfBillReadingControl_readingItemRepeater_trFirstReading1_0')
+        links = []
+        for a in tr.find_all('td')[1].find_all('a'):
+            links.append(a['href'])
+        return(links)
+    except Exception as e:
+        return([])
 
 
 def get_house_bills():
@@ -125,3 +136,4 @@ def get_senate_bills():
 # d = get_house_bills()
 # df = pd.DataFrame(d)
 # df.to_csv("lowerbills.csv")
+print(get_bill_text_links('https://www.aph.gov.au/Parliamentary_Business/Bills_Legislation/Bills_Search_Results/Result?bId=r6356'))
