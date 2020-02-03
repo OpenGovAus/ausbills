c251d8e7fa7b7f8327dcb701e273377eaecdddb9
KipCrossing <kip.crossing@gmail.com>
2020-02-03 21:54:23 +1100

    Fetching data from dicts

--- ausbills/federal_theyvote.py
+++ ausbills/federal_theyvote.py
@@ -1,12 +1,19 @@
 import os
 import json
 import requests
+import pandas as pd
+
 
 API_KEY = os.environ.get('THEYVOTE_API_KEY', None)
 
-print(API_KEY)
-
 response = requests.get('https://theyvoteforyou.org.au/api/v1/people.json?key='+API_KEY)
 
+current_people = json.loads(response.content)
 
-print(response.content)
+"""
+{'id': 10947, 'latest_member': {'id': 100906, 'name': {'first': 'Perin', 'last': 'Davey'}, 'electorate': 'NSW', 'house': 'senate', 'party':
+'National Party'}}
+"""
+
+for person in current_people:
+    print(person['id'], person['latest_member']['name']['first'],person['latest_member']['name']['last'])
