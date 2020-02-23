68317346302d7f5a05fc65429d46f12c23612316
KipCrossing <kip.crossing@gmail.com>
2020-02-23 22:10:08 +1100

    upgrade version

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ausbills", # Replace with your own username
-    version="0.0.1",
+    name="ausbills",  # Replace with your own username
+    version="0.0.2",
     author="Kipling Crossing",
     author_email="kip.crossing@gmail.com",
     description="Get parliament bills for Australian governments.",
