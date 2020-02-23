7637d014847be9157056157bd977d3ab8b7581e9
KipCrossing <kip.crossing@gmail.com>
2020-02-23 16:39:55 +1100

    Bills class

--- ausbills/federal_bills.py
+++ ausbills/federal_bills.py
@@ -2,9 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import asyncio
+import datetime
 
 
-CHAMBER = "CHAMBER"
+CHAMBER = "Chamber"
 SHORT_TITLE = "Short Title"
 INTRO_HOUSE = "Intro House"
 PASSED_HOUSE = "Passed House"
@@ -15,170 +16,145 @@ URL = "URL"
 ACT_NO = "Act No."
 SUMMARY = "Summary"
 
-
 bills_legislation_url = "https://www.aph.gov.au/Parliamentary_Business/Bills_Legislation/Bills_Lists/Details_page?blsId=legislation%2fbillslst%2fbillslst_c203aa1c-1876-41a8-bc76-1de328bdb726"
 
 
-def get_table_data(tds, col):
-    try:
-        if tds[col].span:
-            return(tds[col].span.string)
+class Federal_Bills(object):
+    _bills_data = []
+    chambers = ["House", "Senate"]
+    this_year = datetime.datetime.now().year
+
+    def __init__(self):
+        self._build_dataset()
+
+    def _build_dataset(self):
+        try:
+            for i in range(2):
+                self._scrape_data(i)
+        except Exception as e:
+            print("Link broken:")
+            print(e)
+
+    def _scrape_data(self, table_no):
+        website_url = requests.get(bills_legislation_url).text
+        soup = BeautifulSoup(website_url, 'lxml')
+        tables = soup.find_all('table')
+        trs = tables[table_no].findAll('tr')
+        tr = trs.pop(0)
+        self.headings = self._get_row_data(tr.findAll('td'))
+
+        for tr in trs:
+            try:
+                bill_url_string = str(tr.a['href'])
+                row_data = self._get_row_data(tr.findAll('td'))
+                row_dict = {CHAMBER: self.chambers[table_no]}
+                for i in range(len(self.headings)):
+                    row_dict[self.headings[i]] = row_data[i]
+                row_dict[URL] = bill_url_string
+                row_dict = self._convert_to_datetime(row_dict)
+                self._bills_data.append(row_dict)
+            except Exception as e:
+                print("Bad data", e)
+
+    def _get_row_data(self, tds):
+        row_data = []
+        for col in range(7):
+            try:
+                if tds[col].span:
+                    row_data.append(tds[col].span.string)
+                else:
+                    row_data.append("")
+            except Exception as e:
+                print(e)
+                row_data.append("")
+        return(row_data)
+
+    def _convert_to_datetime(self, bill_dict):
+        bill_year = self.this_year
+        for i in range(6):
+            year = self.this_year - i
+            if str(year) in bill_dict[SHORT_TITLE]:
+                bill_year = year
+        if bill_dict[INTRO_HOUSE] != "":
+            intro_house = bill_dict[INTRO_HOUSE].split('/')
+            bill_dict[INTRO_HOUSE] = datetime.date(
+                bill_year, int(intro_house[1]), int(intro_house[0]))
         else:
-            return("")
-    except Exception as e:
-        print(e)
-        return("")
+            bill_dict[INTRO_HOUSE] = None
 
-
-def get_bill_summary(bill_url_string):
-    try:
-        bill_url = requests.get(bill_url_string).text
-        bill_soup = BeautifulSoup(bill_url, 'lxml')
-        div = bill_soup.find("div", id='main_0_summaryPanel')
-    except Exception as e:
-        div = None
-    if div:
-        for span_tag in div.find_all('span'):
-            span_tag.unwrap()
-        summary = div.p.text.replace('\n', '').replace('    ', '')
-    else:
-        summary = ""
-    return(summary)
-
-
-def get_bill_text_links(bill_url_string):
-    try:
-        bill_url = requests.get(bill_url_string).text
-        bill_soup = BeautifulSoup(bill_url, 'lxml')
-        tr = bill_soup.find(
-            "tr", id='main_0_textOfBillReadingControl_readingItemRepeater_trFirstReading1_0')
-        links = []
-        for a in tr.find_all('td')[1].find_all('a'):
-            links.append(a['href'])
-        return(links)
-    except Exception as e:
-        return([])
-
-
-def get_bill_em_links(bill_url_string):
-    try:
-        bill_url = requests.get(bill_url_string).text
-        bill_soup = BeautifulSoup(bill_url, 'lxml')
-        tr = bill_soup.find(
-            "tr", id='main_0_explanatoryMemorandaControl_readingItemRepeater_trFirstReading1_0')
-        links = []
-        for a in tr.find_all('td')[1].find_all('a'):
-            links.append(a['href'])
-        return(links)
-    except Exception as e:
-        return([])
-
-
-def get_sponsor(bill_url_string):
-    try:
-        bill_url = requests.get(bill_url_string).text
-        bill_soup = BeautifulSoup(bill_url, 'lxml')
-        tr = bill_soup.find("div", id='main_0_billSummary_sponsorPanel')
-        return(tr.find_all('dd')[0].text.replace(' ', '').replace('\n', ''))
-    except Exception as e:
-        return('')
-
-
-class lower_house_bills(object):
-    LHB = []
-    def __init__(self):
-
-        try:
-            website_url = requests.get(bills_legislation_url).text
-            soup = BeautifulSoup(website_url, 'lxml')
-            tables = soup.find_all('table')
-            trs = tables[0].findAll('tr')
-            trs.pop(0)
-
-            for tr in trs:
-                try:
-                    bill_url_string = str(tr.a['href'])
-                    title = get_table_data(tr.findAll('td'), 0)
-                    intro_house = get_table_data(tr.findAll('td'), 1)
-                    passed_house = get_table_data(tr.findAll('td'), 2)
-                    intro_senate = get_table_data(tr.findAll('td'), 3)
-                    passed_senate = get_table_data(tr.findAll('td'), 4)
-                    assent_date = get_table_data(tr.findAll('td'), 5)
-                    act_no = get_table_data(tr.findAll('td'), 6)
-                    self.LHB.append(
-                        {
-                            CHAMBER: "House",
-                            SHORT_TITLE: title,
-                            INTRO_HOUSE: intro_house,
-                            PASSED_HOUSE: passed_house,
-                            INTRO_SENATE: intro_senate,
-                            PASSED_SENATE: passed_senate,
-                            ASSENT_DATE: assent_date,
-                            URL: bill_url_string,
-                            ACT_NO: act_no})
-                except Exception as e:
-                    print("Link broken:")
-
-        except Exception as e:
-            print("Link broken:")
-            print(e)
-            print(title)
+        passed_house = bill_dict[PASSED_HOUSE].split('/')
+        into_senaet = bill_dict[INTRO_SENATE].split('/')
+        passed_senate = bill_dict[PASSED_SENATE].split('/')
+        print(bill_year, bill_dict[INTRO_HOUSE], bill_dict[SHORT_TITLE])
+        return(bill_dict)
 
     @property
     def data(self):
-        return(self.LHB)
+        return(self._bills_data)
 
 
-class upper_house_bills(object):
-    UHB = []
+class Bill(object):
 
-    def __init__(self):
+    def __init__(self, initial_data):
+        self.initial_data = initial_data
+
+    def get_bill_summary(self, bill_url_string):
         try:
-            website_url = requests.get(bills_legislation_url).text
-            soup = BeautifulSoup(website_url, 'lxml')
-            tables = soup.find_all('table')
-            trs = tables[1].findAll('tr')
-            trs.pop(0)
-
-            for tr in trs:
-                try:
-                    bill_url_string = str(tr.a['href'])
-                    title = get_table_data(tr.findAll('td'), 0)
-                    intro_house = get_table_data(tr.findAll('td'), 3)
-                    passed_house = get_table_data(tr.findAll('td'), 4)
-                    intro_senate = get_table_data(tr.findAll('td'), 1)
-                    passed_senate = get_table_data(tr.findAll('td'), 2)
-                    assent_date = get_table_data(tr.findAll('td'), 5)
-                    act_no = get_table_data(tr.findAll('td'), 6)
-                    self.UHB.append(
-                        {
-                            CHAMBER: "Senate",
-                            SHORT_TITLE: title,
-                            INTRO_HOUSE: intro_house,
-                            PASSED_HOUSE: passed_house,
-                            INTRO_SENATE: intro_senate,
-                            PASSED_SENATE: passed_senate,
-                            ASSENT_DATE: assent_date,
-                            URL: bill_url_string,
-                            ACT_NO: act_no})
-                except Exception as e:
-                    print("Bad data")
-
+            bill_url = requests.get(bill_url_string).text
+            bill_soup = BeautifulSoup(bill_url, 'lxml')
+            div = bill_soup.find("div", id='main_0_summaryPanel')
         except Exception as e:
-            print("Link broken:")
-            print(e)
-            print(title)
+            div = None
+        if div:
+            for span_tag in div.find_all('span'):
+                span_tag.unwrap()
+            summary = div.p.text.replace('\n', '').replace('    ', '')
+        else:
+            summary = ""
+        return(summary)
 
-    @property
-    def data(self):
-        return(self.UHB)
+    def get_bill_text_links(self, bill_url_string):
+        try:
+            bill_url = requests.get(bill_url_string).text
+            bill_soup = BeautifulSoup(bill_url, 'lxml')
+            tr = bill_soup.find(
+                "tr", id='main_0_textOfBillReadingControl_readingItemRepeater_trFirstReading1_0')
+            links = []
+            for a in tr.find_all('td')[1].find_all('a'):
+                links.append(a['href'])
+            return(links)
+        except Exception as e:
+            return([])
+
+    def get_bill_em_links(self, bill_url_string):
+        try:
+            bill_url = requests.get(bill_url_string).text
+            bill_soup = BeautifulSoup(bill_url, 'lxml')
+            tr = bill_soup.find(
+                "tr", id='main_0_explanatoryMemorandaControl_readingItemRepeater_trFirstReading1_0')
+            links = []
+            for a in tr.find_all('td')[1].find_all('a'):
+                links.append(a['href'])
+            return(links)
+        except Exception as e:
+            return([])
+
+    def get_sponsor(self, bill_url_string):
+        try:
+            bill_url = requests.get(bill_url_string).text
+            bill_soup = BeautifulSoup(bill_url, 'lxml')
+            tr = bill_soup.find("div", id='main_0_billSummary_sponsorPanel')
+            return(tr.find_all('dd')[0].text.replace(' ', '').replace('\n', ''))
+        except Exception as e:
+            return('')
+
+# for testing
 
 
-lhb = lower_house_bills()
-uhb = upper_house_bills()
+fb = Federal_Bills()
 
-print(lhb.data[2])
+print(fb.data[3])
 print()
-print(uhb.data[2])
+url = fb.data[3]["URL"]
+print(url)
 print()
-print(get_sponsor('https://www.aph.gov.au/Parliamentary_Business/Bills_Legislation/Bills_Search_Results/Result?bId=r6356'))
