26fab43bafdaa98ed00989cad58cf1ad22b55fa2
KipCrossing <kip.crossing@gmail.com>
2020-03-20 21:47:50 +1100

    Tinker circleci setting

--- setup.py
+++ setup.py
@@ -9,20 +9,6 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-class VerifyVersionCommand(install):
-    """Custom command to verify that the git tag matches our version"""
-    description = 'verify that the git tag matches our version'
-
-    def run(self):
-        tag = os.getenv('CIRCLE_TAG')
-
-        if tag != VERSION:
-            info = "Git tag: {0} does not match the version of this app: {1}".format(
-                tag, VERSION
-            )
-            sys.exit(info)
-
-
 setuptools.setup(
     name="ausbills",  # Replace with your own username
     version=VERSION,
@@ -39,7 +25,4 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    cmdclass={
-        'verify': VerifyVersionCommand,
-    }
 )
