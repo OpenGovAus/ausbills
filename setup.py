f67dde14b8469b13ccf9122d250b7b585ccf5a98
KipCrossing <kip.crossing@gmail.com>
2020-03-20 19:47:41 +1100

    Prepare setup for CD

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
 import setuptools
+import os
+import sys
+from setuptools.command.install import install
+
+VERSION = "0.0.8"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+
 setuptools.setup(
     name="ausbills",  # Replace with your own username
-    version="0.0.8",
+    version=VERSION,
     author="Kipling Crossing",
     author_email="kip.crossing@gmail.com",
     description="Get parliament bills for Australian governments.",
@@ -19,4 +39,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )
