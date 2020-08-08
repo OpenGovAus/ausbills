f1b602203eb6def256a2fee24de75bb1d4e306df
connorhsm <connorhsmith@gmail.com>
2020-08-08 22:15:58 +1000

    Scraping WA bills

--- ausbills/wa_parliment.py
+++ ausbills/wa_parliment.py
@@ -2,14 +2,30 @@ from bs4 import BeautifulSoup
 import requests
 import datetime
 
-bills_legislation_url = "https://www.parliament.wa.gov.au/parliament/bills.nsf/screenWebCurrentBills"
-
+URL = "https://www.parliament.wa.gov.au/parliament/bills.nsf/WebAllBills?openview&start=1&count=3000"
 
 class All_Bills(object):
     _bills_data = []
 
     def __init__(self):
-        pass
+        self._build_dataset()
+
+    def _build_dataset(self):
+        try:
+            self._scrape_data()
+
+        except Exception as e:
+            print(f'Link broken: {e}')
+
+    def _scrape_data(self):
+        page = requests.get(URL, verify=False)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        table = soup.find('table')
+        rows = table.findAll('tr')
+        # Remove table heading
+        rows.pop(0)
+
+        print(rows)
 
     @property
     def data(self):
