3c27d0257a37ff34a0523fd66f69f16cc57b845a
king-millez <millez.dev@gmail.com>
2020-10-16 19:32:57 +1100

    Add bill URL and ID scraping

---
+++ ausbills/qld_parliament.py
@@ -0,0 +1,31 @@
+from requests import get
+import json
+Geimport re
+
+bill_list_url = 'https://www.legislation.qld.gov.au/projectdata?ds=OQPC-BrowseDataSource&start=1&count=9999&sortField=sort.title&sortDirection=asc&expression=Repealed%3DN+AND+PrintType%3D(%22bill.first%22+OR+%22bill.firstnongovintro%22)+AND+Title%3Da%20OR%20b%3F&subset=browse&collection=&_=1602801890379'
+
+class qld_All_Bills(object):
+    _bills_data = []
+
+    def __init__(self):
+        try:
+            self._create_dataset()
+        except:
+            raise Exception('Error when scraping lists...')
+
+    def _create_dataset(self):
+        data = json.loads(get(bill_list_url).text)
+        _bill_urls = []
+        _bill_ids = []
+
+        for bill in range(len(data['data'])):
+            if(bill % 2 != 0):
+                if 'bill-' in data['data'][bill]['id']['__value__'] and not 'https://www.legislation.qld.gov.au/view/html/bill.first/' + data['data'][bill]['id']['__value__'] in _bill_urls:
+                    _bill_urls.append('https://www.legislation.qld.gov.au/view/html/bill.first/' + data['data'][bill]['id']['__value__'])
+                    _bill_ids.append(data['data'][bill]['id']['__value__'])
+
+    @property
+    def data(self):
+        return(self._bills_data)
+
+qld_all_bills = qld_All_Bills().data
