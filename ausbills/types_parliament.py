b784ff29e52cbeeda7b015a548fc346ff20d0761
kipling <kip.crossing@gmail.com>
2020-10-29 11:33:06 +1100

    Add Parliament types

--- ausbills/types_parliament.py
+++ ausbills/types_parliament.py
@@ -1,6 +1,17 @@
 from enum import Enum
 
 
+class Parliament(Enum):
+    FEDERAL = "FEDERAL"
+    NSW = "NSW"
+    WA = "WA"
+    NT = "NT"
+    QLD = "QLD"
+    VIC = "VIC"
+    SA = "SA"
+    TAS = "TAS"
+
+
 class House(Enum):
     UPPER = "UPPER"
     LOWER = "LOWER"
